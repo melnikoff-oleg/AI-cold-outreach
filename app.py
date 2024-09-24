bc9a5bb73e6f541d3776662b8e0fafa7b358c2a3
Oleg Melnikov <oleghools@gmail.com>
2024-09-24 11:56:34 +0400

    make prompts better, add jupyter notebook for fast experiments

--- app.py
+++ app.py
@@ -26,11 +26,14 @@ if submit_button:
         company_profile = requests.get(api_endpoint, params=params, headers=headers).json()
         
         prompt = ''
-        prompt += f"""You are a professional B2B sales manager with 10 years of experience. Help me write a cold outreach message to a potential client, to make them interested in my services. 
+        prompt += f"""You are a professional B2B sales manager with 10 years of experience, you are an expert in writing cold messages that convert.
+        Help me write a cold outreach message to a potential client, to make them interested in talking to me. 
         ---
-        Here is a description of my services:
+        Here is a description of my intention:
         I'm a tech expert with decent experience in software development and artificial intellegence. I quit my job and started my own company to help businesses automate their processes using AI, make their employees more efficient, save time and money. Right now I'm looking for the first company to partner with, as I want to build up my portfolio and help someone at the same time.
-        I would love to develop a custom AI solution for your company. And I'm happy to guide you in the world of AI if you need it. Examples of solutions I can develop: 1) automate analysis of something and data gathering, 2) help writing personalized cold outreach messages, 3) analyse performance of your employees and give recommendations on how to improve it, 4) automate any sort of routine tasks, 5) build a chatbot for any purpose, 6) build an internal documentation system with an easy search, 7) many other AI solutions applicable to your business.
+        I would love to develop a custom AI solution for your company. And I'm happy to guide you in the world of AI if you need it. 
+        Examples of solutions I can develop: 1) automate data gathering and data analysis, 2) personalize content for a specific person based on their intent, 3) analyse performance of your employees and give recommendations on how to improve it, 4) automate any sort of routine tasks, 5) build a chatbot for any purpose, 6) build an internal documentation system with an easy search, 7) many other AI solutions applicable to your business.
+        In my message I wanna convey that initially I just wanna to talk, to discuss potential AI applications in their case, I don't wanna pretend being an expert, I don't wanna be sellsy, I want to have a friendly conversation first of all.
         ---\n"""
         prompt += f"""Here is the profile of the lead you are reaching out to:
         Name: {person_profile['full_name']}. Occupation: {person_profile['occupation']}. Summary: {person_profile['summary']}
@@ -38,14 +41,14 @@ if submit_button:
         Name: {company_profile['name']}. Industry: {company_profile['industry']}. Description: {company_profile['description']}
         ---\n"""
         prompt += f"""Help me write a message for this lead that resonates with him and makes him interested in my service. Keep in mind my requirements:
-        - showcase a problem that their company might have and how my service can solve it
-        - introduce myself and my services
+        - showcase a problem that their company might have and how AI can solve it
+        - introduce myself and what I want for them
         - keep it short and to the point, at max 100 words
         - make the message personalized, but don't be dishonest or insincere, don't you ever say you followed their career
         - make attention-grabbing opening
         - showcase how they can make more profit by integrating AI
-        - showcase that I'm happy to navigate and consult them
-        - add a very clear call to action: ask if the lead wants to know more about what I can do specifically for them. so they can answer just YES. don't ask for a meeting or a call
+        - showcase that I'm happy to navigate and just talk to them
+        - add a very clear call to action: ask if they have tried and found value in using AI to improve their processes.
         - WRITE IMPERFECTLY, LIKE A HUMAN. IF THE PROSPECT WILL RECOGNISE YOU AS A BOT, THEY WILL KILL YOU
         At first brainstorm what might be specific problems and solutions the lead might face. Reason and make a chain of thought. Then write the message itself.
         ---\n"""
